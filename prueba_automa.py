173097dca7856dddae67da59ed933a463ca43664
JoanMdv <joan_martinez.olivares@hotmail.com>
2024-02-15 11:08:53 -0600

    Se hizo volvio al codigo anterior por problemas con la variable my_mail

--- prueba_automa.py
+++ prueba_automa.py
@@ -3,6 +3,7 @@ import email
 import yaml
 import re
 import pandas as pd
+from openpyxl import load_workbook
 from datetime import datetime
 from twilio.rest import Client
 import time
@@ -13,159 +14,145 @@ logging.basicConfig(filename='correo_script.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
 
 
-def cargar_credenciales(file_path):
-    # funcion para leer las Credenciales en el archivo yaml
-    with open(file_path) as f:
-        content = f.read()
-    return yaml.load(content, Loader=yaml.FullLoader)
+# Abrir el archivo donde se encuentran las credenciales
+with open("CREDENTIALS.yml") as f:
+    content = f.read()
 
+# Trabajar con los datos del archivo YAML
+my_credentials = yaml.load(content, Loader=yaml.FullLoader)
 
-def conectar_gmail(user, password, imap_url="imap.gmail.com"):
-    # funcion para conectar_gmail con user , password , imap_url
-    my_mail = imaplib.IMAP4_SSL(imap_url)
-    my_mail.login(user, password)
-    # iniciamos serion en gmail con el user y password
-    my_mail.select("inbox")
-    return my_mail
-    # retornamos la bandeja que ocuparemos
+# Declarar las variables de usuario y contraseña
+user, password = my_credentials["user"], my_credentials["password"]
 
+# URL IMAP
+imap_url = "imap.gmail.com"
 
-def buscar_ultimo_correo(my_mail, key, value):
-    # funcion que retorna el ultimo correo , con el key y value
-    _, data = my_mail.search(None, key, value)
-    # Buscar en la bandeja los correos relacionados con 'value'
-    mail_id_list = data[0].split()
-    # Seleccionar y separar el numero del correo electr√≥nico
-    msgs = []
-    if mail_id_list:
-        last_mail_id = mail_id_list[-1]
-        typ, last_mail_data = my_mail.fetch(last_mail_id, '(RFC822)')
-        msgs.append(last_mail_data)
-    else:
-        logging.info(
-            "No se encontraron correos electrónicos que cumplan con los criterios.")
-    return msgs
-    # retornamos el ultimo correo electronico deseado
-
-
-def extraer_datos_correo(msgs):
-    data = {'Subject': [], 'From': [], 'Body': []}
-    # Crear un DataFrame para almacenar los datos
-    for msg in msgs[::-1]:
-        # Iterar sobre los mensajes recuperados y extraer informacion
-        for response_part in msg:
-            if type(response_part) is tuple:
-                my_msg = email.message_from_bytes(response_part[1])
-
-                # Agregar informacion al DataFrame
-                data['Subject'].append(" 📎 : " + my_msg['subject'])
-                data['From'].append(" 📬 : " + my_msg['from'])
-
-                body = ''
-                for part in my_msg.walk():
-                    if part.get_content_type() == 'text/plain':
-                        body = part.get_payload()
-                
-                # Codificar y formatear el cuerpo del mensaje modificacion google
-                body = body.encode('utf-8').decode('unicode_escape')
-                body = re.sub(r'\s+', ' ', body)
-                data['Body'].append(body)
-    return pd.DataFrame(data)
-    # Crear un DataFrame con los datos
-
-
-def cargar_datos_excel(excel_path, sheet_name):
+while True:
     try:
-        df_existing = pd.read_excel(excel_path, sheet_name=sheet_name)
-        # Cargar la hoja de Excel existente en el DataFrame
-    except FileNotFoundError:
-        # Si el archivo no existe, crea un DataFrame vacío
-        df_existing = pd.DataFrame(columns=['Subject', 'From', 'Body'])
-    return df_existing
+        # Conexión a Gmail mediante SSL
+        my_mail = imaplib.IMAP4_SSL(imap_url)
 
+        # Iniciar sesión en Gmail
+        my_mail.login(user, password)
 
-def enviar_mensaje_whatsapp(ultima_fila, nueva_columna_nombre, account_sid, auth_token, numero_celular):
-    asunto = ultima_fila['Subject']
-    remitente = ultima_fila['From']
-    cuerpo = ultima_fila['Body']
-
-    mensaje_whatsapp = (
-        f"📩 *Nuevo Correo Electrónico* 📩 \n"
-        f"Asunto: {asunto}\n"
-        f"Remitente: {remitente}\n"
-        f"Cuerpo del Correo: {cuerpo}"
-    )
-
-    columnas_seleccionadas = ultima_fila[
-        [nueva_columna_nombre, 'Subject', 'From', 'Body']]
-    mensaje_whatsapp = columnas_seleccionadas.to_string(index=False)
-    mensaje_whatsapp = mensaje_whatsapp.encode('utf-8').decode('utf-8')
-    partes_mensaje = [mensaje_whatsapp[i:i+1500]
-                      for i in range(0, len(mensaje_whatsapp), 1500)]
-
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        from_='whatsapp:+14155238886',
-        body=partes_mensaje,
-        to=numero_celular
-    )
-
-    logging.info(f"Mensaje de WhatsApp enviado: {message.sid}")
-
-
-def procesar_correos(user, password, excel_path, sheet_name, account_sid, auth_token, numero_celular):
-    try:
-        my_mail = conectar_gmail(user, password)
+        # Seleccionar la bandeja de entrada
+        my_mail.select("inbox")
 
         key = "from"
         value = "joan_martinez.olivares@hotmail.com"
-        msgs = buscar_ultimo_correo(my_mail, key, value)
 
-        if msgs:
-            df = extraer_datos_correo(msgs)
-            df_existing = cargar_datos_excel(excel_path, sheet_name)
-            df_combined = pd.concat([df_existing, df], ignore_index=True)
+        # Buscar en la bandeja los correos relacionados con 'value'
+        _, data = my_mail.search(None, key, value)
 
-            nueva_columna_nombre = 'NuevaColumna'
-            nuevo_valor = f"📩 NUEVO 📩"
-            df_combined[nueva_columna_nombre] = nuevo_valor
+        # Seleccionar y separar el número del correo electrónico
+        mail_id_list = data[0].split()
 
-            ultima_fila = df_combined.iloc[-1]
+        msgs = []  # Lista para almacenar todos los mensajes
 
-            if not df_existing.equals(df_combined):
-                enviar_mensaje_whatsapp(
-                    ultima_fila, nueva_columna_nombre, account_sid, auth_token, numero_celular)
+        # Extraer solo el último correo completo deseado
+        if mail_id_list:
+            last_mail_id = mail_id_list[-1]
+            typ, last_mail_data = my_mail.fetch(last_mail_id, '(RFC822)')
+            msgs.append(last_mail_data)
+        else:
+            logging.info(
+                "No se encontraron correos electrónicos que cumplan con los criterios.")
 
-                df_combined.to_excel(
-                    excel_path, sheet_name=sheet_name, index=False)
+        # Crear un DataFrame para almacenar los datos
+        data = {'Subject': [], 'From': [], 'Body': []}
+
+        # Iterar sobre los mensajes recuperados y extraer información
+        for msg in msgs[::-1]:
+            for response_part in msg:
+                if type(response_part) is tuple:
+                    my_msg = email.message_from_bytes((response_part[1]))
+
+                    # Agregar información al DataFrame
+                    data['Subject'].append(" 🔎 : " + my_msg['subject'])
+                    data['From'].append(" 📭 : " + my_msg['from'])
+
+                    body = ''
+                    for part in my_msg.walk():
+                        if part.get_content_type() == 'text/plain':
+                            body = part.get_payload()
+
+                    # Codificar y formatear el cuerpo del mensaje
+                    body = body.encode('utf-8').decode('unicode_escape')
+                    body = re.sub(r'\s+', ' ', body)
+                    data['Body'].append(body)
+
+        # Crear un DataFrame con los datos
+        df = pd.DataFrame(data)
+
+        # Cargar el archivo Excel existente
+        excel_path = 'emails.xlsx'
+        book = load_workbook(excel_path)
+
+        # Obtener el nombre de la hoja
+        sheet_name = 'Hoja1'  # Cambia esto al nombre de tu hoja existente si es diferente
+
+        # Cargar la hoja de Excel existente en el DataFrame
+        df_existing = pd.read_excel(excel_path, sheet_name=sheet_name)
+
+        # Concatenar el nuevo DataFrame con el existente
+        df_combined = pd.concat([df_existing, df], ignore_index=True)
+
+        # Agregar nueva columna
+        nueva_columna_nombre = 'NuevaColumna'
+        nuevo_valor = f"📩 NUEVO 📩"
+        df_combined[nueva_columna_nombre] = nuevo_valor
+
+        # Obtener la última fila del DataFrame combinado
+        ultima_fila = df_combined.iloc[-1]
+
+        # Personalizar el mensaje de WhatsApp
+        asunto = ultima_fila['Subject']
+        remite = ultima_fila['From']
+        cuerpo = ultima_fila['Body']
+
+        # Construir el mensaje de WhatsApp con emojis y formato estético
+        mensaje_whatsapp = (
+            f"📩 *Nuevo Correo Electrónico* 📩 \n"
+            f"Asunto: {asunto}\n"
+            f"Remitente: {remite}\n"
+            f"Cuerpo del Correo: {cuerpo}"
+        )
+
+        # Enviar mensaje por WhatsApp para la última fila
+        columnas_seleccionadas = ultima_fila[[
+            nueva_columna_nombre, 'Subject', 'From', 'Body',]]
+        mensaje_whatsapp = columnas_seleccionadas.to_string(index=False)
+        hora_actual = datetime.now().time()
+        mensaje_whatsapp = mensaje_whatsapp.encode('utf-8').decode('utf-8')
+        partes_mensaje = [mensaje_whatsapp[i:i+1500]
+                          for i in range(0, len(mensaje_whatsapp), 1500)]
+
+        account_sid = 'AC3430b29f9d9b9f403701741fafdf1150'
+        auth_token = my_credentials["auth_token"]
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            from_='whatsapp:+14155238886',
+            body=partes_mensaje,
+            to=my_credentials['numero_celular']
+        )
+
+        logging.info(f"Mensaje de WhatsApp enviado: {message.sid}")
+        df_combined.to_excel(excel_path, sheet_name=sheet_name, index=False)
 
     except Exception as e:
         logging.error(f"Error: {e}")
 
     finally:
         try:
+            # Cerrar la conexión
             my_mail.close()
         except:
             pass
         my_mail.logout()
 
+    # Registrar la hora de la ejecución
+    logging.info(f"Ejecutado a las {datetime.now()}")
 
-if __name__ == "__main__":
-    my_credentials = cargar_credenciales("CREDENTIALS.yml")
-    user, password = my_credentials["user"], my_credentials["password"]
-    excel_path = 'emails.xlsx'
-    sheet_name = 'Hoja1'
-    account_sid = 'AC3430b29f9d9b9f403701741fafdf1150'
-    auth_token = my_credentials["auth_token"]
-    numero_celular = my_credentials['numero_celular']
-
-    while True:
-        try:
-            procesar_correos(
-                user, password, excel_path, sheet_name, account_sid, auth_token, numero_celular)
-
-        except Exception as e:
-            logging.error(f"Error: {e}")
-
-        logging.info(f"Ejecutado a las {datetime.now()}")
-        time.sleep(60)
+    # Procesar nuevos correos cada 1 minutos (ajusta según tus necesidades)
+    time.sleep(60)  # Esperar 1 minutos antes de volver a revisar
